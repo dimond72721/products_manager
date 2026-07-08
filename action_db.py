94f2bae1bcce27968de9ee36b0dee7adf47d3409
dimond72721 <dimond14122009@gmail.com>
2026-07-08 16:58:55 +0300

    Implement CRUD operations for Product model

--- action_db.py
+++ action_db.py
@@ -1,1 +1,34 @@
+from models import Product
 
+'''create'''
+def add_product(name: str, price: float, category: str):
+    Product.create(name=name, price=price, category=category)
+
+'''read'''
+
+def get_categories():
+    products = Product.select(Product.category).distinct().order_by(Product.category)
+    categories = [product.category for product in products]
+    return categories
+
+def get_products():
+    return Product.select()
+
+def get_products_by_category(category: str):
+    return Product.select().where(Product.category == category)
+
+def product_exists(name: str) -> bool:
+    return Product.select().where(Product.name == name).exists()
+
+
+'''update'''
+def edit_product(name: str, price: float, category: str):
+    product = Product.get(Product.name == name)
+    product.price = price
+    product.category = category
+    product.save()
+
+
+'''delete'''
+def delete_product(name: str):
+    Product.delete().where(Product.name == name).execute()
