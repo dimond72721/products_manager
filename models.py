34a372973302cc0d77e5b3a83a765c1cc09a0078
dimond72721 <dimond14122009@gmail.com>
2026-07-08 17:29:15 +0300

    Add Product model with database setup

--- models.py
+++ models.py
@@ -1,1 +1,16 @@
+from peewee import *
 
+db = SqliteDatabase("products.db")
+
+
+class Product(Model):
+    name = CharField(unique=True)
+    price = FloatField()
+    category = CharField()
+
+    class Meta:
+        database = db
+
+
+db.connect()
+db.create_tables([Product])
